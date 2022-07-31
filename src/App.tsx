71abb2c4dd365527b6b40cf6d5e683c03839b6e1
Shubhanshu <shubh.aarsay@gmail.com>
2022-07-30 22:45:14 -0400

    Prettier run

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import * as React from "react";
+import * as React from 'react';
 import { render } from 'react-dom';
 const App = () => {
-  return(
-    <h1 className={"text-red-600"}>Hello World</h1>
-  );
-}
+  return <h1 className={'text-red-600'}>Hello World</h1>;
+};
 render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
-)
+);
